d13de8498c892903bf998a538a43ad4f68703ded
RyuTaeuk <65931758+RyuTaeuk@users.noreply.github.com>
2023-02-28 15:56:33 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int result[9] = { 0, };
+
+void searching(int cnt) {
+    if (cnt == m) {
+        for (int i = 0; i < m; i++)
+            cout << result[i] << ' ';
+        cout << '\n';
+        return;
+    } // m 사이즈의 수열이 완성되었을때 return
+
+    for (int i = 1; i <= n; i++) {
+        if (cnt == 0 || result[cnt - 1] <= i) {
+            result[cnt] = i;
+            searching(cnt + 1);
+        }
+    } //수열을 만드는 과정
+}
+
+int main() {
+    cin >> n >> m;
+    searching(0);
+
+    return 0;
+}
