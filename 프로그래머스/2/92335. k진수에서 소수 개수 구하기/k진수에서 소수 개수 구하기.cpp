521890a3875d0aa3ae2e5176da123a61c4b1f25d
RyuTaeuk <65931758+RyuTaeuk@users.noreply.github.com>
2026-09-24 18:32:27 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 2.76 ms, Memory: 4.79 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+string to_k_base(int n, int k) {
+    string res = "";
+    while(n > 0) {
+        res += to_string(n % k);
+        n /= k;
+    }
+    
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+bool isPrime(long long num) {
+    if(num < 2) return false;
+    for(long long i = 2; i*i <= num; i++) {
+        if(num % i == 0) return false;
+    }
+    return true;
+}
+
+int solution(int n, int k) {
+    int answer = 0;
+    string str = to_k_base(n, k);
+    stringstream ss(str);
+    string token;
+    while(getline(ss, token, '0')) {
+        if(token.empty()) continue;
+        
+        long long num = stoll(token);
+        if(isPrime(num)) {
+            answer++;
+        }
+    }
+    
+    return answer;
+}
