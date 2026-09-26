7e012015057a41a8cf4a5d27d9792e68d4260b85
RyuTaeuk <65931758+RyuTaeuk@users.noreply.github.com>
2026-09-26 20:24:04 +0900

    [level 2] Title: [3차] n진수 게임, Time: 0.61 ms, Memory: 4.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+// 모든 숫자를 구한다?
+// to string?
+// p, 2p, 3p... 번째
+string to_n_base(int n, int num) {
+    string res = "";
+    if (num == 0) return "0";
+    while(num > 0) {
+        if(num % n > 9) {
+            res += 'A' + (num % n - 10);
+        }
+        else {
+            res += to_string(num % n);
+        }
+        num /= n;
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
+string solution(int n, int t, int m, int p) {
+    string answer = "";
+    string str = "";
+    int i = 0;
+    while(str.size() < t*m) {
+        str += to_n_base(n, i);
+        i++;
+    }
+    
+    for(int i=0; i<t; i++) {
+        answer += str[m*i+p-1];
+    }
+    return answer;
+}
