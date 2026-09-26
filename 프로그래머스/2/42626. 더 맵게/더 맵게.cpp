2413024087693e341c86bee2163c879d164c315e
RyuTaeuk <65931758+RyuTaeuk@users.noreply.github.com>
2026-09-26 15:42:12 +0900

    [level 2] Title: 더 맵게, Time: 195.80 ms, Memory: 41.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    
+    for(int n : scoville) {
+        pq.push(n);
+    }
+    
+    while(pq.top() < K) {
+        if(pq.size() < 2) {
+            answer = -1;
+            break;
+        }
+        int num1 = pq.top();
+        pq.pop();
+        int num2 = pq.top();
+        pq.pop();
+        int new_scoville = num1 + 2 * num2;
+        pq.push(new_scoville);
+        answer++;
+    }
+    return answer;
+}
